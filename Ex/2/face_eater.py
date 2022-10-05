6a46f7396d6b8ee3c4eb845613062b673b88b08a
Tobi <jimyouare@gmail.com>
2022-10-05 17:09:49 +0200

    ex2

---
+++ Ex/2/face_eater.py
@@ -0,0 +1,12 @@
+# face-eater 
+
+# using face_recognition
+# https://github.com/ageitgey/face_recognition
+
+
+
+import os
+
+cmd = "face_recognition ./known_people/ ./unkown_people/"
+os.system(cmd)
+
